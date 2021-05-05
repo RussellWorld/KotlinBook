f911c3f7036c554b1ab60d5f2f391fa27c49f7c9
Russell <miraqq55@gmail.com>
2021-05-05 07:40:17 +0200

    Theme 4

--- src/Game.kt
+++ src/Game.kt
@@ -1,3 +1,5 @@
+import kotlin.math.pow
+
 fun main() {
     val name = "Mandrigal"
     var healthPoint = 78
@@ -5,6 +7,21 @@ fun main() {
     val isImmortal = false
     val race = "gnome"
 
+    //Определение рассы
+    whatRace(race)
+
+    //Аура
+    val auraColor = auraColor(isBlessed, healthPoint, isImmortal)
+
+    val healthStatus = formatHealthStatus(healthPoint, isBlessed)
+    //Состояние игрока
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+
+    castFireball(4)
+
+}
+
+private fun whatRace(race: String) {
     val faction = when (race) {
         "gnome" -> "Keepers of the Mines"
         "dwarf" -> "Keepers of the Mines"
@@ -12,21 +29,45 @@ fun main() {
         "human" -> "Free People of the Rolling Hills"
         else -> "Error race"
     }
+}
 
-    //Aura
-    val auraVisible = isBlessed && healthPoint > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
-
-    val healthStatus = when (healthPoint) {
-        100 -> "is in excellent condition!"
-        in 90..99 -> "has a few scratches."
-        in 75..89 -> if (isBlessed) "has some minor wounds is healing quite quickly!" else "has some minor wounds."
-        in 15..74 -> "looks pretty hurts."
-        else -> "is in awful condition!"
-
-    }
-
+private fun printPlayerStatus(
+    auraColor: String,
+    isBlessed: Boolean,
+    name: String,
+    healthStatus: String
+) {
     println("(Aura: $auraColor Blessed: ${if (isBlessed) "YES" else "NO"})")
     println("$name $healthStatus")
-
 }
+
+private fun auraColor(
+    isBlessed: Boolean,
+    healthPoint: Int,
+    isImmortal: Boolean
+): String {
+    val auraVisible = isBlessed && healthPoint > 50 || isImmortal
+    return if (auraVisible) "GREEN" else "NONE"
+}
+
+private fun formatHealthStatus(healthPoint: Int, isBlessed: Boolean) = when (healthPoint) {
+    100 -> "is in excellent condition!"
+    in 90..99 -> "has a few scratches."
+    in 75..89 -> if (isBlessed) "has some minor wounds is healing quite quickly!" else "has some minor wounds."
+    in 15..74 -> "looks pretty hurts."
+    else -> "is in awful condition!"
+}
+
+private fun castFireball(count: Int = 2) {
+    val i = count * 7
+    val status = when (i) {
+        in 1..10 -> "Tipsy"
+        in 11..20 -> "Sloshed"
+        in 21..30 -> "Soused"
+        in 31..40 -> "Stewed"
+        else -> "t0st3d"
+    }
+    println("A glass of Fireball springs into existence. (x$count) (Power: $i) = Status enemy: $status")
+}
+
+
