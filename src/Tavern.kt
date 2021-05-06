06913021e023ab437ddac03f886b7db27fb452a9
Russell <miraqq55@gmail.com>
2021-05-06 06:39:12 +0200

    Theme 7

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,15 +1,35 @@
+const val TAVERN_NAME = "Taernyl's Folly"
+
 fun main() {
-    var beverage = readLine()
-    beverage = null
-    if (beverage != null)
-        beverage = beverage.capitalize()
-    else
-        println("I can't do that without - beverage is null!")
 
-    val beverageServed: String = beverage ?: "Buttered Alle"
-    println(beverageServed)
+//    placeOrder("shandy,Dragon's Breath,5.91")
+    placeOrder("elixir,Shirley's Temple,4.12")
 }
 
-fun readLine(): String? {
-    return "beverage"
+fun toDragonsSpeak(pharse: String) =
+    pharse.replace(Regex("[aeiou]")) {
+        when (it.value) {
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
+
+fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order.")
+
+    val (type, name, price) = menuData.split(",")
+    val message = "Madrigal buys a $name ($type) for $price."
+    println(message)
+
+    val pharse = if (name == "Dragon's Breath")
+        "Madrigal exclaims: ${toDragonsSpeak("Ah, delicious $name!")}"
+    else "Madrigal says: Thanks for the $name."
+    print(pharse)
 }
+
